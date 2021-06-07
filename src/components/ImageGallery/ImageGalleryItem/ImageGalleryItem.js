6442efd539d3b24c04c5f096da6fa2bec3fb54b3
Pavel Ivchenko <71591602+Chivapchichi2@users.noreply.github.com>
2021-06-08 01:38:01 +0300

    test3

--- src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGallery/ImageGalleryItem/ImageGalleryItem.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './ImageGalleryItem.module.css';
-// eslint-disable-next-line
+
 const ImageGalleryItem = ({ poster, title, vote }) => (
   <li className={styles.ImageGalleryItem}>
     <img
-      // eslint-disable-next-line
       src={`https://image.tmdb.org/t/p/w300${poster}`}
       alt=""
       // data-url={largeImageURL}
@@ -19,7 +18,7 @@ const ImageGalleryItem = ({ poster, title, vote }) => (
 ImageGalleryItem.propTypes = {
   poster: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
-  vote: PropTypes.string.isRequired,
+  vote: PropTypes.number.isRequired,
   // tags: PropTypes.string.isRequired,
   // largeImageURL: PropTypes.string.isRequired,
 };
